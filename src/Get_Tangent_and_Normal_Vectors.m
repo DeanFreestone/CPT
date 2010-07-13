d43124eabb489f070eb67b0d96b17f5d93f56ba1
Dean Freestone <dean@uom-69117.ee.mu.oz.au>
2010-07-14 09:52:55 +1000

    removed line fitting from tangent function

--- src/Get_Tangent_and_Normal_Vectors.m
+++ src/Get_Tangent_and_Normal_Vectors.m
@@ -18,27 +18,12 @@ function [Simple_Tangent Simple_Norm] = Get_Tangent_and_Normal_Vectors(Samples_F
     center = floor(length(y_s_all)/2) + 1;              % length of y_s_all should be odd so there is a centre
     
     Samples_Either_Side = floor(Samples_For_Tangent/2);
-    
-%     y_t = y_s_all(center - Samples_Either_Side:center + Samples_Either_Side);
-%     Hy_t = Hy_s_all(center - Samples_Either_Side:center + Samples_Either_Side);
-% 
-%     b_ls = regress(Hy_t',[ones(size(y_t))' y_t']);            % y=ax+b or y = p(1) + p(2)x
-%     tangent = b_ls(2)*y_t + b_ls(1);
-% 
-%     % find the normal vector (z) of the estimated tangent
-%     z = [y_t(end)-y_t(1) tangent(end)-tangent(1)];
-%     a = [z(2); -z(1)];                             % (normal to the separating line) translated to the origin
+
     
     
     Simple_Tangent = [y_s_all(center + Samples_Either_Side) - y_s_all(center - Samples_Either_Side)...
     Hy_s_all(center + Samples_Either_Side) - Hy_s_all(center - Samples_Either_Side)];
     
     Simple_Norm = [Simple_Tangent(2); -Simple_Tangent(1)];
-
-%     Simple_Tangent = Simple_Tangent/norm(Simple_Tangent);
-%     Simple_Norm = Simple_Norm/norm(Simple_Norm);
-%     
-%     z = Simple_Tangent;
-%     a = Simple_Norm;
 
 end
